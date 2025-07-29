6ce21a7b6e05b1e9673553fae2ee2ea36391ae11
Matheus Kepy <matheuskdev@gmail.com>
2025-07-29 00:14:14 -0300

    feat: Adiciona casos de uso para criação de documentos

---
+++ src/core/application/use_cases/base.py
@@ -0,0 +1,21 @@
+"""Use Cases Base"""
+
+from abc import ABC, abstractmethod
+
+from src.core.application.services.base import IDocumentService
+from src.core.domain.entities.document import Document
+from src.core.domain.repositorys.document import IDocumentRepository
+
+
+class UseCase(ABC):
+    """Classe base para todos os casos de uso."""
+
+    @abstractmethod
+    def execute(
+        self,
+        repository: IDocumentRepository,
+        service: IDocumentService,
+        document: Document,
+    ) -> Document:
+        """Método que executa o caso de uso."""
+        raise NotImplementedError
---
+++ src/core/application/use_cases/create_document.py
@@ -0,0 +1,29 @@
+"""Use case para criar um documento."""
+
+from core.application.services.base import IDocumentService
+from src.core.application.use_cases.base import UseCase
+from src.core.domain.entities.document import Document
+from src.core.domain.events.document import DocumentCreatedEvent
+from src.core.domain.repositorys.document import IDocumentRepository
+
+
+class CreateDocumentUseCase(UseCase):
+    """Caso de uso para criar um documento."""
+
+    def execute(
+        self,
+        repository: IDocumentRepository,
+        service: IDocumentService,
+        document: Document,
+    ) -> Document:
+        """Executa o caso de uso para criar um documento."""
+
+        service.validate(document)
+        document = repository.save(document)
+
+        event = DocumentCreatedEvent(
+            document.id, document.user_id, document.document_type.value
+        )
+        document.add_domain_event(event)
+
+        return document
