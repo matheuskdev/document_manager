ccc6fde8e363ec0e0754c5a6b363498d3513f30f
Matheus Kepy <matheuskdev@gmail.com>
2025-07-28 23:26:39 -0300

    fix: Adiciona herança da classe ABC na interface IDocumentRepository

--- src/core/domain/repositorys/document.py
+++ src/core/domain/repositorys/document.py
@@ -1,34 +1,41 @@
 """Repository para a entidade Document."""
 
+from abc import ABC, abstractmethod
 from uuid import UUID
 
 from src.core.domain.entities.document import Document
 from src.core.domain.value_objects.doc_types import DocumentType
 
 
-class IDocumentRepository:
+class IDocumentRepository(ABC):
     """Interface para o repositório de documentos."""
 
+    @abstractmethod
     def add(self, document: Document) -> None:
         """Adiciona um documento ao repositório."""
         raise NotImplementedError
 
+    @abstractmethod
     def get(self, document_id: UUID) -> Document:
         """Obtém um documento do repositório."""
         raise NotImplementedError
 
+    @abstractmethod
     def update(self, document: Document) -> None:
         """Atualiza um documento no repositório."""
         raise NotImplementedError
 
+    @abstractmethod
     def delete(self, document_id: UUID) -> None:
         """Remove um documento do repositório."""
         raise NotImplementedError
 
+    @abstractmethod
     def get_by_id(self, document_id: UUID) -> Document:
         """Obtém um documento pelo ID."""
         raise NotImplementedError
 
+    @abstractmethod
     def get_by_type(self, document_type: DocumentType) -> list[Document]:
         """Obtém documentos pelo tipo."""
         raise NotImplementedError
