76cdc73926ce4a2edbc82e9abbc4abcfb4a0fc97
Matheus Kepy <matheuskdev@gmail.com>
2025-07-28 23:16:57 -0300

    feat: Adiciona exceções do domínio para gerenciamento de documentos

---
+++ src/core/domain/exceptions.py
@@ -0,0 +1,24 @@
+"""Exceções do domínio."""
+
+
+class DocumentNotFoundException(Exception):
+    """Exceção lançada quando um documento não é encontrado."""
+
+
+class DocumentAlreadyExistsException(Exception):
+    """Exceção lançada quando um documento já existe."""
+
+
+class InvalidDocumentTypeException(Exception):
+    """Exceção lançada quando o tipo de documento é inválido."""
+
+
+class DocumentUpdateAttrException(AttributeError):
+    """Exceção lançada quando ocorre um erro ao atualizar um documento."""
+
+
+class DocumentUpdateTypeException(TypeError):
+    """
+    Exceção lançada quando o tipo de dado do atributo a ser atualizado
+    é inválido.
+    """
