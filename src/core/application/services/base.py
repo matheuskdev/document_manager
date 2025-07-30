170e0caee03aaafe921e699cd7b5970e393d611a
Matheus Kepy <matheuskdev@gmail.com>
2025-07-30 17:46:53 -0300

    fix: Adiciona a importação do módulo abc na interface IDocumentService

--- src/core/application/services/base.py
+++ src/core/application/services/base.py
@@ -1,11 +1,14 @@
 """Interface para os serviços de domínio."""
 
+from abc import ABC, abstractmethod
+
 from src.core.domain.entities.document import Document
 
 
-class IDocumentService:
+class IDocumentService():
     """Interface para o serviço de documentos."""
 
+    @abstractmethod
     def validate(self, document: Document) -> None:
         """Valida um documento."""
         raise NotImplementedError
