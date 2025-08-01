26a651e041a115699808e635be93b7f57235b435
Matheus Kepy <matheuskdev@gmail.com>
2025-08-01 02:23:13 -0300

    feat: Adiciona validações e melhorias nos métodos da entidade Document

--- src/core/domain/entities/document.py
+++ src/core/domain/entities/document.py
@@ -1,4 +1,4 @@
-"""Entidade de Documento"""
+"""Entidade de Documento."""
 
 from typing import Any
 from uuid import UUID
@@ -35,46 +35,78 @@ class Document(Entity):
         self.document_type = document_type
         self.user_id = user_id
 
-    def __str__(self):
-        return f"Document(id={self.id}, document_type={self.document_type})"
+    @property
+    def title(self) -> str:
+        """Retorna o título do documento."""
+        return self._title
 
-    def update(
-        self, attr: str, new_value: Any, event_update: DocumentUpdatedEvent
+    @title.setter
+    def title(self, value: str):
+        """Define o título do documento,
+        garantindo que não seja vazio.
+        """
+        if not value:
+            raise ValueError("O título do documento não pode ser vazio.")
+        self._title = value
+
+    @property
+    def document_type(self) -> DocumentType:
+        """Retorna o tipo do documento."""
+        return self._document_type
+
+    @document_type.setter
+    def document_type(self, value: DocumentType):
+        """Define o tipo do documento,
+        garantindo que seja uma instância de DocumentType.
+        """
+        if not isinstance(value, DocumentType):
+            raise DocumentTypeException(
+                "O tipo de documento deve ser uma instância de DocumentType."
+            )
+        self._document_type = value
+
+    @property
+    def user_id(self) -> UUID:
+        """Retorna o ID do usuário que criou o documento."""
+        return self._user_id
+
+
+    def __str__(self):
+        return (
+            f"Document(id={self.id}, document_type={self.document_type.value})"
+        )
+
+    def update_attribute(
+        self, attr: str, new_value: Any, user_id_modifier: UUID
     ):
         """
-        Atualiza um atributo do documento e marca a entidade como modificada.
-
-        Args:
-            attr (str): Nome do atributo a ser atualizado.
-            new_value (Any): Novo valor para o atributo.
-            event_update (DomainEvent): Evento de atualização a ser registrado.
-
-        Returns:
-            None
+        Atualiza um atributo do documento e registra o evento de domínio.
         """
-        obj_property = attr
-
-        if not hasattr(self, obj_property):
+        if not hasattr(self, attr):
             raise DocumentUpdateAttrException(
-                f"<{self.__class__.__name__}> não tem o atributo <{attr}>"
+                f"A entidade '{self.__class__.__name__}'"
+                f"não tem o atributo '{attr}'."
             )
 
-        old_value = getattr(self, obj_property)
+        old_value = getattr(self, attr)
 
-        if not isinstance(new_value, type(old_value)):
-            raise DocumentTypeException(
-                f"Tipo de dado inválido para o atributo: <{attr}>. "
-                f"Esperado: <{type(old_value).__name__}>, "
-                f"Recebido: <{type(new_value).__name__}>"
-            )
-        setattr(self, obj_property, new_value)
+        if old_value == new_value:
+            return
+
+        try:
+            setattr(self, attr, new_value)
+        except Exception as e:
+            raise DocumentUpdateAttrException(
+                f"Falha ao atualizar '{attr}': {e}"
+            ) from e
+
         self._update_timestamp()
         self.add_domain_event(
-            event_update(
-                self.id,
-                self.user_id,
-                old_value,
-                new_value,
-                self.document_type.value,
+            DocumentUpdatedEvent(
+                document_id=self.id,
+                user_id=user_id_modifier,
+                old_value=old_value,
+                new_value=new_value,
+                document_type=self.document_type,
             )
         )
