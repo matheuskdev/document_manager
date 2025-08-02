647c1185492c4dc016282ccd393d9701f8fd4502
Matheus Kepy <matheuskdev@gmail.com>
2025-08-02 00:40:24 -0300

    feat: Adiciona novos métodos ao repositório de documentos para busca por usuário, status e tenant

--- src/core/domain/repositorys/document.py
+++ src/core/domain/repositorys/document.py
@@ -31,11 +31,43 @@ class IDocumentRepository(ABC):
         raise NotImplementedError
 
     @abstractmethod
+    def all(self) -> list[Document]:
+        """Obtém todos os documentos do repositório."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def count(self) -> int:
+        """Conta o número de documentos no repositório."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def exists(self, document_id: UUID) -> bool:
+        """Verifica se um documento existe no repositório."""
+        raise NotImplementedError
+
+    @abstractmethod
     def get_by_id(self, document_id: UUID) -> Document:
         """Obtém um documento pelo ID."""
         raise NotImplementedError
 
     @abstractmethod
-    def get_by_type(self, document_type: DocumentType) -> list[Document]:
+    def get_by_document_type(
+        self, document_type: DocumentType
+    ) -> list[Document]:
         """Obtém documentos pelo tipo."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_user_id(self, user_id: UUID) -> list[Document]:
+        """Obtém documentos associados a um usuário específico."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_status(self, status: str) -> list[Document]:
+        """Obtém documentos pelo status."""
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_by_tenant_id(self, tenant_id: UUID) -> list[Document]:
+        """Obtém documentos associados a um tenant específico."""
         raise NotImplementedError
