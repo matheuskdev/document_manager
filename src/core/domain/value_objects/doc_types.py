2029aa4749d387accceb56559b0efc20f0878bac
Matheus Kepy <matheuskdev@gmail.com>
2025-08-05 01:02:27 -0300

    docs: Atualiza a docstring da classe DocumentType para incluir valores e descrições dos tipos de documentos

--- src/core/domain/value_objects/doc_types.py
+++ src/core/domain/value_objects/doc_types.py
@@ -4,7 +4,17 @@ from enum import Enum
 
 
 class DocumentType(Enum):
-    """Enumeração para tipos de documentos."""
+    """Enumeração para tipos de documentos.
+    
+    Values:
+        REPORT: Relatório.
+        CONTRACT: Contrato.
+        PROTOCOL: Protocolo.
+        POP: Procedimento Operacional Padrão.
+        TUTORIAL: Tutorial.
+        MANUAL: Manual.
+        OTHER: Outro.
+    """
 
     REPORT = "Relatório"
     CONTRACT = "Contrato"
